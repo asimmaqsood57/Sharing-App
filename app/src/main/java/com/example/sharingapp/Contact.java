a294d8e6b3cbdd9383f8d40bfa544af9d2236f98
M.Asim <asimmaqsood57@gmail.com>
2021-05-10 23:13:35 +0500

    All are done

---
+++ app/src/main/java/com/example/sharingapp/Contact.java
@@ -0,0 +1,37 @@
+package com.example.sharingapp;
+import java.util.UUID;
+public class Contact {
+    private String username;
+    private String email;
+    private String id;
+    public Contact(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+        if (id == null) {
+            setId();
+        } else {
+            updateId(id);
+        }
+    }
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
+    }
+    public void updateId(String id) {
+        this.id = id;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setUsername() {
+        this.username = UUID.randomUUID().toString();
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setEmail() {
+        this.email = UUID.randomUUID().toString();
+    }
+    public String getEmail(){
+        return email;
+    }
+}
